1e10f61759404f4c3c446669bf59a1de01d43880
Cyril <cyril.personne@outlook.fr>
2024-04-17 07:32:35 +0200

    push

--- src/main/java/fr/traitementfichier/ApplicationOpenFoodFacts.java
+++ src/main/java/fr/traitementfichier/ApplicationOpenFoodFacts.java
@@ -12,38 +12,48 @@ public class ApplicationOpenFoodFacts {
         System.out.println("Génération du stock....");
         Stock stock = ReadFile.getStock();
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Entrez le numéro du mode que vous souhaitez\n");
-        System.out.print("1 - Rechercher les meilleurs produits pour une Marque donnée\n");
-        System.out.print("2 - Rechercher les meilleurs produits pour une Catégorie donnée\n");
-        System.out.print("3 - Rechercher les meilleurs produits par Marque et par Catégorie\n");
-        System.out.print("4 - Afficher les allergènes les plus courants avec le nb de produits dans lesquels ils apparaissent.\n");
-        System.out.print("5 - Afficher les additifs les plus courants avec le nb de produits dans lesquels ils apparaissent.");
+        boolean continuer = true;
+        while (continuer) {
 
-        int choix = scanner.nextInt();
-        scanner.nextLine();
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Entrez le numéro du mode que vous souhaitez\n");
+            System.out.print("1 - Rechercher les meilleurs produits pour une Marque donnée\n");
+            System.out.print("2 - Rechercher les meilleurs produits pour une Catégorie donnée\n");
+            System.out.print("3 - Rechercher les meilleurs produits par Marque et par Catégorie\n");
+            System.out.print("4 - Afficher les allergènes les plus courants avec le nb de produits dans lesquels ils apparaissent.\n");
+            System.out.print("5 - Afficher les additifs les plus courants avec le nb de produits dans lesquels ils apparaissent.");
+            scanner.nextLine();
 
-        switch (choix) {
-            case 1:
-                RechercheMeilleurProduitsMarque rechercheProduitsMarque = new RechercheMeilleurProduitsMarque();
-                rechercheProduitsMarque.traiter(stock, scanner);
-                break;
-            case 2:
-                RechercheMeilleurProduitsCategorie rechercheProduitsCategorie = new RechercheMeilleurProduitsCategorie();
-                rechercheProduitsCategorie.traiter(stock, scanner);
-                break;
-            case 3:
-                RechercheMeilleurProduitsMarqueEtCategorie rechercheProduitsMarqueCategorie = new RechercheMeilleurProduitsMarqueEtCategorie();
-                rechercheProduitsMarqueCategorie.traiter(stock, scanner);
-                break;
-            case 4:
-                AfficherAllergenesPlusCourants afficherAllergenesPlusCourants = new AfficherAllergenesPlusCourants();
-                afficherAllergenesPlusCourants.traiter(stock, scanner);
-                break;
-            case 5:
-                AfficherAdditifsPlusCourants afficherAdditifsPlusCourants = new AfficherAdditifsPlusCourants();
-                afficherAdditifsPlusCourants.traiter(stock, scanner);
-                break;
+            int choix = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choix) {
+                case 1:
+                    RechercheMeilleurProduitsMarque rechercheProduitsMarque = new RechercheMeilleurProduitsMarque();
+                    rechercheProduitsMarque.traiter(stock, scanner);
+                    break;
+                case 2:
+                    RechercheMeilleurProduitsCategorie rechercheProduitsCategorie = new RechercheMeilleurProduitsCategorie();
+                    rechercheProduitsCategorie.traiter(stock, scanner);
+                    break;
+                case 3:
+                    RechercheMeilleurProduitsMarqueEtCategorie rechercheProduitsMarqueCategorie = new RechercheMeilleurProduitsMarqueEtCategorie();
+                    rechercheProduitsMarqueCategorie.traiter(stock, scanner);
+                    break;
+                case 4:
+                    AfficherAllergenesPlusCourants afficherAllergenesPlusCourants = new AfficherAllergenesPlusCourants();
+                    afficherAllergenesPlusCourants.traiter(stock, scanner);
+                    break;
+                case 5:
+                    AfficherAdditifsPlusCourants afficherAdditifsPlusCourants = new AfficherAdditifsPlusCourants();
+                    afficherAdditifsPlusCourants.traiter(stock, scanner);
+                    break;
+            }
+            System.out.println("Voulez-vous choisir une autre option du menu ? (oui/non)");
+            String reponse = scanner.nextLine();
+            if (!reponse.equalsIgnoreCase("oui")) {
+                continuer = false;
+            }
         }
     }
 }
